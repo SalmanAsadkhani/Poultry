ebb8b0cdd73ace4ef54f7af4b9b3a8ef257d70f7
itsSalmanAsd <153323750+SalmanAsadkhani@users.noreply.github.com>
2025-07-27 16:52:25 +0330

    logout

--- app/Http/Controllers/auth/LoginController.php
+++ app/Http/Controllers/auth/LoginController.php
@@ -30,6 +30,8 @@ class LoginController extends Controller {
             return back()->with('error','نام کاربری یا رمز عبور اشتباه است ');
         }
 
+
+
         Auth::login($user, true);
 
         return redirect()->route('home');
@@ -41,24 +43,8 @@ class LoginController extends Controller {
 
     public function logout(){
 
-        addLog('خروج از حساب کاربری');
-
-        if (Auth::check()) {
-            $user = User::where('id',auth()->user()->id)->first();
-            $user->should_re_login = 1;
-            $user->save();
-            Auth::logout();
-        }
-        if(Auth::guard('admin')->check()) {
-            $user = User::where('id',auth('admin')->user()->id)->first();
-            $user->should_re_login = 1;
-            $user->save();
-            Auth::guard('admin')->logout();
-        }
-        session()->remove('isShahrdar');
-        session()->forget(['user_id','user','isShahrdar']);
-
-        return redirect()->route('home');
+        Auth::logout();
+        return redirect()->route('login');
 
     }
 
--- routes/auth.php
+++ routes/auth.php
@@ -7,28 +7,17 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\ForgotController;
 
-
-Route::prefix('register')->group(function () {
-    Route::get('', [RegisterController::class, 'index'])->name('register');
-    Route::post('', [RegisterController::class, 'save'])->name('register');
-
-    Route::get('verify', [RegisterController::class, 'verify'])->name('register_verify');
-    Route::post('verify', [RegisterController::class, 'verify_save'])->name('register_verify');
-
-    Route::get('resend', [RegisterController::class, 'resend'])->name('register_resend');
-});
-
 Route::prefix('login')->group(function () {
     Route::get('', [LoginController::class, 'index'])->name('login');
-    Route::post('', [LoginController::class, 'login'])->name('login');
+    Route::post('', [LoginController::class, 'login'])->name('login')->middleware( 'throttle:5,1');
 });
 
-Route::prefix('forgot')->group(function () {
-    Route::get('', [ForgotController::class, 'index'])->name('forgot');
-    Route::post('', [ForgotController::class, 'checkMobile'])->name('forgot');
+//Route::prefix('forgot')->group(function () {
+//    Route::get('', [ForgotController::class, 'index'])->name('forgot');
+//    Route::post('', [ForgotController::class, 'checkMobile'])->name('forgot');
+//
+//    Route::get('verify', [ForgotController::class, 'verifyPage'])->name('forgot_verify');
+//    Route::post('verify', [ForgotController::class, 'checkCode'])->name('forgot_verify');
+//});
 
-    Route::get('verify', [ForgotController::class, 'verifyPage'])->name('forgot_verify');
-    Route::post('verify', [ForgotController::class, 'checkCode'])->name('forgot_verify');
-});
-
-Route::any('logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('logout', [LoginController::class, 'logout'])->name('logout');
